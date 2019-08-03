4b4f68aa7347218a7a83f14e2e693156a04571f7
David <davidangulococruera@gmail.com>
2019-08-04 01:11:58 +0200

    working in ads

--- app/src/main/java/com/cloudfy/warInstructions/home/HomeFragment.kt
+++ app/src/main/java/com/cloudfy/warInstructions/home/HomeFragment.kt
@@ -10,9 +10,10 @@ import androidx.navigation.fragment.navArgs
 import com.cloudfy.warInstructions.R
 import com.cloudfy.warInstructions.base.BaseFragment
 import com.cloudfy.warInstructions.entities.Chapter
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
 import kotlinx.android.synthetic.main.fragment_home.*
-
-
+import org.jetbrains.anko.support.v4.act
 
 
 class HomeFragment : BaseFragment() {
@@ -29,6 +30,13 @@ class HomeFragment : BaseFragment() {
         chapters = ArrayList(args.chapters.toList())
 
         bundle.putParcelableArray("chapters", chapters.toTypedArray())
+
+        MobileAds.initialize(act, "ca-app-pub-1767954011690390~3917587805")
+
+
+        val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
+
 
         btnIndex.setOnClickListener {
             this.findNavController().navigate(R.id.goToIndex, bundle)
